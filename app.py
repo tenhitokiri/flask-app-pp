5c986a70d7d110a8e359a8ff1a89063a9e1ee37d
Christian Mejia <tenhitokiri@gmail.com>
2019-12-26 21:05:56 -0400

    funcionalidad finalizada, falta mejoras visuales

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -20,32 +20,46 @@ class BlogPost(db.Model):
         return 'Blog post: ' + str(self.id)
 
 
-all_posts = [
-    {
-        'title': 'post 1',
-        'content': 'este es el contenido',
-        'author': 'thk'
-    },
-    {
-        'title': 'post 2',
-        'content': 'este es el contenido del post 2'
-    }
-]
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(
+            title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
 
-@app.route('/strings/<string:name>')
-def hello(name):
-    return "Hello," + name
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post_id = BlogPost.query.get_or_404(id)
+    db.session.delete(post_id)
+    db.session.commit()
+    return redirect('/posts')
+
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    post = BlogPost.query.get_or_404(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.content = request.form['content']
+        post.author = request.form['author']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html', post=post)
 
 
 @app.route('/onlyget', methods=['POST'])
